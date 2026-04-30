0a36b58c34a5a7a31fba2f6d315699c878d7bf84
yousseforabi279 <yousseforabi279@gmail.com>
2026-05-01 00:12:53 +0300

    Add ReceptionAppointments

--- GraduationProject.Data/Enums/AppointmentType.cs
+++ GraduationProject.Data/Enums/AppointmentType.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 namespace GraduationProject.Data.Enums
 {
-   
+
 
     public enum AppointmentType
     {
